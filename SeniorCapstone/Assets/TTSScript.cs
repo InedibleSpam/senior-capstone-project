5f0bca5b299d0a1d16ca4a9b8b26575d5d33d63a
Samuel <samuel.cobb27@houghton.edu>
2026-03-24 08:29:45 -0400

    Finished edits on Ray interactable

--- SeniorCapstone/Assets/TTSScript.cs
+++ SeniorCapstone/Assets/TTSScript.cs
@@ -19,4 +19,19 @@ public class TTSScript : MonoBehaviour
             Debug.LogError("TTSSpeaker component not found on this GameObject!");
         }
     }
+      public void SpeakPatches()
+    {
+        if (speaker != null)
+        {
+            speaker.Speak("These are patches.");
+        }
+    }
+     void OnTranscription(string text)
+    {
+        Debug.Log("Heard: " + text);
+
+        // Respond using TTS
+        if(text.Contains("hello"))
+            speaker.Speak("Hi there! Nice to meet you.");
+    }
 }
