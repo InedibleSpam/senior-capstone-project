96fcc5088af84deccc1403216742a111e506e33e
Samuel <samuel.cobb27@houghton.edu>
2026-03-31 09:28:43 -0400

    Added ray interactable to user and some objects for testing, as well as written TTS scripts for ojbects in half the rooms

---
+++ SeniorCapstone/Assets/Scripts/Cupola_TTS.cs
@@ -0,0 +1,43 @@
+using Meta.WitAi.TTS.Utilities;
+using UnityEngine;
+
+public class Cupola_TTS : MonoBehaviour
+{
+    private TTSSpeaker speaker;
+
+    void Start()
+    {
+        // Automatically find TTSSpeaker on the same GameObject
+        speaker = GetComponent<TTSSpeaker>();
+
+        if (speaker == null)
+        {
+            Debug.LogError("TTSSpeaker component not found on this GameObject!");
+        }
+    }
+    public void SpeakByID(string id)
+{
+    if (speaker == null) return;
+
+    switch (id)
+    {
+        case "mssav":
+            speaker.Speak("This is the MSSAV.");
+            break;
+
+        case "bottomwindow":
+            speaker.Speak("This is the bottom window.");
+            break;
+    }
+}   
+
+    void OnTranscription(string text)
+    {
+        Debug.Log("Heard: " + text);
+
+        // Respond using TTS
+        if(text.Contains("hello"))
+            speaker.Speak("Hi there! Nice to meet you.");
+    }
+    
+}
